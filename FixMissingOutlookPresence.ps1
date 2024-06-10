d15ee22842c73b4a3c9cc2eb51dff7b347a140e6
Shaun Emmons <s1e8@pge.com>
2024-06-10 16:37:22 -0700

    adding Teams Clear Cache and Fix Teams Presence Scripts

---
+++ FixMissingOutlookPresence.ps1
@@ -0,0 +1,62 @@
+﻿#
+# Script to add Outlook Presence Files
+#
+
+CLEAR
+
+# Check if Teams is running
+$teamsProcesses = Get-Process -Name "*Teams" -ErrorAction SilentlyContinue
+$countTeams = ($teamsProcesses | Measure-Object).Count
+Write-Host "Count of Teams processes: $($countTeams)"
+
+if($countTeams -gt 0)
+{
+    Write-Host "You must close all Teams processes for the Outlook Presence to be fixed." -ForegroundColor Red
+    $yestNo = Read-Host "Would you like this script to force close all Teams proceses? (y/n)"
+    
+    if("y" -eq $yestNo -or "yes" -eq $yestNo)
+    {
+        Write-Host "Closing Teams..."
+        Stop-Process -Name "*Teams" -Force -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 10
+    }
+}
+
+
+# Get all Outlook processes
+$outlookProcesses = Get-Process Outlook -ErrorAction SilentlyContinue
+Write-Host "Outlook processes will now be forced closed to perform Outlook Presence fix." -ForegroundColor Red
+
+# Check if any Outlook processes are found
+if ($outlookProcesses) {
+    # Close all Outlook processes
+    $outlookProcesses | ForEach-Object { $_.CloseMainWindow() | Out-Null }
+    Write-Host "Outlook processes have been closed."
+} else {
+    # No Outlook processes found
+    Write-Host "No Outlook processes are running."
+}
+
+# Check if TeamsPresenceAddin Folder is Present and add tlb files if needed
+
+$parentPath = $env:LOCALAPPDATA
+$childPath ="Microsoft\TeamsPresenceAddin"
+
+$destinationPath = Join-Path -Path $parentPath -ChildPath $childPath
+
+
+# Check if the folder exists
+if (-not (Test-Path -Path $destinationPath)) {
+    Write-Host "Creating folder: $($destinationPath)"
+    New-Item -ItemType Directory -Path $destinationPath | Out-Null
+}
+
+# Copy tlb files to Destination Folder
+
+$source = "\\PCCS01\O365ClientScripts$\Outlook_Presence_Fix\*"
+Copy-Item -Path $source -Destination $destinationPath
+Write-Host "Folder created. Presence files added" -ForegroundColor Red
+
+Write-Host "Process complete. Script can be closed." -ForegroundColor Red
+
+Read-Host -Prompt "Press enter to close"
---
+++ TeamsClearCache.ps1
@@ -0,0 +1,79 @@
+﻿#
+# Script to clear the Teams cache
+#
+
+CLEAR
+
+# Load all the cache locations that need to be cleared.
+$cacheFolders = @()
+
+$cacheFolders += Join-Path -Path $env:LOCALAPPDATA -ChildPath "packages\msteams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\*"
+
+
+# Check if Teams is running
+$teamsProcesses = Get-Process -Name "*Teams" -ErrorAction SilentlyContinue
+$countTeams = ($teamsProcesses | Measure-Object).Count
+Write-Host "Count of Teams processes: $($countTeams)"
+
+if($countTeams -gt 0)
+{
+    Write-Host "You must close all Teams processes for the cache clear to be performed." -ForegroundColor Red
+    $yestNo = Read-Host "Would you like this script to force close all Teams proceses? (y/n)"
+    
+    if("y" -eq $yestNo -or "yes" -eq $yestNo)
+    {
+        Write-Host "Closing Teams..."
+        Stop-Process -Name "*Teams" -Force -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 10
+    }
+}
+
+$teamsProcesses = Get-Process -Name "Teams" -ErrorAction SilentlyContinue
+$countTeams = ($teamsProcesses | Measure-Object).Count
+
+if($countTeams -gt 0)
+{
+    Write-Host "Unable to close all Teams processes. Script ending." -ForegroundColor Red
+}
+else
+{
+    $fileCount = 0
+    $successCount = 0
+    $errorCount = 0
+    foreach($cache in $cacheFolders)
+    {
+        if(Test-Path -Path $cache)
+        {
+            $filesToDelete = Get-ChildItem -Path $cache -Recurse -ErrorAction SilentlyContinue
+            
+            foreach($f in $filesToDelete)
+            {
+                if( -not $f.PSIsContainer)
+                {
+                    $fileCount++
+                    try
+                    {
+                        Write-Host "Deleting file: $($f.FullName)"
+                        Remove-Item $($f.FullName) -Force -ErrorAction Stop
+                        Write-Host "`tSuccess" -ForegroundColor Green
+                        $successCount++
+                    }
+                    catch
+                    {
+                        Write-Host "`tERROR: $($_)" -ForegroundColor Red
+                        $errorCount++
+                    }
+                }
+            }
+        }
+    }
+    
+    Write-Host ""
+    Write-Host "Results:"
+    Write-Host "Count of files to remove = $($fileCount)"
+    Write-Host "Count of success = $($successCount)"
+    Write-Host "Count of errors = $($errorCount)"
+    Write-Host ""
+}
+
+Read-Host "Process complete. Press Enter to exit the script"
